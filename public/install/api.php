131621631604f6a0d9d68d6238132588a26014ee
Ben Thomson <git@alfreido.com>
2021-04-30 14:11:16 +0800

    Move API into install folder

--- public/install/api.php
+++ public/install/api.php
@@ -3,7 +3,7 @@
 use Winter\Installer\Api;
 
 // Autoload Composer dependencies
-require_once __DIR__ . '/install/api/vendor/autoload.php';
+require_once __DIR__ . '/api/vendor/autoload.php';
 
 $api = new Api;
 $api->request();
