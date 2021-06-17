fc5f7e462bdfd3aa1c3773af9be4de2a1622418d
Ben Thomson <git@alfreido.com>
2021-06-17 22:10:41 +0800

    Change buttons on completion step to links

--- src/components/steps/Complete.vue
+++ src/components/steps/Complete.vue
@@ -8,37 +8,45 @@
       <p>Your new installation is ready to go. We hope you enjoy using Winter CMS!</p>
     </div>
     <div class="step-actions">
-      <Click
-        label="View your install"
-        flag="primary"
-        @press="viewSite"
-      />
+      <a
+        :href="siteUrl"
+        target="wintercms"
+        class="btn btn-primary"
+      >
+        View your install
+      </a>
 
-      <Click
-        label="View your Backend"
-        flag="primary"
-        @press="viewBackend"
-      />
+      <a
+        :href="backendUrl"
+        target="backend"
+        class="btn btn-primary"
+      >
+        View your Backend
+      </a>
     </div>
   </div>
 </template>
 
 <script>
 import StepMixin from '@/mixins/step';
-import Click from '@/components/Click.vue';
 
 export default {
   name: 'Complete',
   mixins: [
     StepMixin,
   ],
-  components: {
-    Click,
-  },
   props: {
     site: {
       type: Object,
       required: true,
+    },
+  },
+  computed: {
+    siteUrl() {
+      return this.site.url;
+    },
+    backendUrl() {
+      return this.site.url + '/' + this.site.backendUrl;
     },
   },
   data() {
@@ -47,18 +55,12 @@ export default {
       stepName: 'Complete',
     };
   },
-  methods: {
-    viewSite() {
-      window.location.replace(this.site.url);
-    },
-    viewBackend() {
-      window.location.replace(this.site.url + '/' + this.site.backendUrl);
-    },
-  },
 };
 </script>
 
 <style lang="scss" scoped>
+@import "~spectre.css/src/mixins/shadow";
+
 .step-content {
   display: flex;
   flex-direction: column;
@@ -102,5 +104,68 @@ export default {
 
 p {
   margin-bottom: 0.6rem;
+}
+
+.btn {
+  appearance: none;
+  background: $bg-color-light;
+  border: none;
+  border-radius: $border-radius;
+  color: $primary-color;
+  cursor: pointer;
+  display: inline-block;
+  font-size: $font-size;
+  height: $control-size;
+  line-height: $line-height;
+  outline: none;
+  padding: $control-padding-y $control-padding-x;
+  text-align: center;
+  text-decoration: none;
+  transition: background .2s, border .2s, box-shadow .2s, color .2s;
+  user-select: none;
+  vertical-align: middle;
+  white-space: nowrap;
+
+  &:focus {
+    @include control-shadow();
+  }
+  &:focus,
+  &:hover {
+    background: $secondary-color;
+    border-color: $primary-color-dark;
+    text-decoration: none;
+  }
+  &:active,
+  &.active {
+    background: $primary-color-dark;
+    border-color: darken($primary-color-dark, 5%);
+    color: $light-color;
+    text-decoration: none;
+    &.loading {
+      &::after {
+        border-bottom-color: $light-color;
+        border-left-color: $light-color;
+      }
+    }
+  }
+
+  // Button Primary
+  &.btn-primary {
+    background: $primary-color;
+    border-color: $primary-color-dark;
+    color: $light-color;
+    &:focus,
+    &:hover {
+      background: darken($primary-color-dark, 2%);
+      border-color: darken($primary-color-dark, 5%);
+      color: $light-color;
+    }
+    &:active,
+    &.active {
+      background: darken($primary-color-dark, 4%);
+      border-color: darken($primary-color-dark, 7%);
+      color: $light-color;
+    }
+  }
 }
 </style>
